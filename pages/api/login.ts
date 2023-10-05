749bf5180f9f567c58c8be39024866c0d221a3d2
Raphael Monteiro de Oliveira Rebello <raphaelsiqueiraduarte@gmail.com>
2023-10-05 11:57:40 -0300

    configuração do banco de dados

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,18 +1,22 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+import {conectaMongoDB} from '../../middlewares/conectaMongoDB';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 
-export default (
+const endpointLogin = (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
         
         if(login === 'admin@admin.com' &&
             senha === 'Admin@123'){
-                res.status(200).json({msg: 'Usuário autenticado com sucesso.'});
+               return res.status(200).json({msg: 'Usuário autenticado com sucesso.'});
             }
             return res.status(400).json({erro: 'Usuário ou Senha inválido(a).'});
 
     }
     return res.status(405).json({erro: 'Método informado não é válido.'});
 }
+
+export default conectaMongoDB(endpointLogin);
