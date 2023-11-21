1e79e3ac36e85094ffd461b28e30a07a07cd8d4e
Raphael Monteiro de Oliveira Rebello <raphaelsiqueiraduarte@gmail.com>
2023-11-21 15:44:32 -0300

    Avatar

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,41 +2,53 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
 import {UsuarioModel} from '../../models/UsuarioModel';
-import {conectaMongoDB} from "../../middlewares/conectaMongoDB"
+import {conectaMongoDB} from '../../middlewares/conectaMongoDB';
 import md5 from "md5";
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
+import nc from 'next-connect';
 
-const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-    if(req.method === 'POST') {
-        const usuario = req.body as CadastroRequisicao;
-
-        if(!usuario.nome || usuario.nome.length <2){
-            return res.status(400).json({erro: 'Nome inválido.'});
-        }
-
-        if(!usuario.email || usuario.email.length <5
-            || !usuario.email.includes('@')
-            || !usuario.email.includes('.')){
-            return res.status(400).json({erro: 'Email inválido.'});    
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+       
+            const usuario = req.body as CadastroRequisicao;
+    
+            if(!usuario.nome || usuario.nome.length <2){
+                return res.status(400).json({erro: 'Nome inválido.'});
+            }
+    
+            if(!usuario.email || usuario.email.length <5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({erro: 'Email inválido.'});    
+                }
+    
+            if(!usuario.senha || usuario.senha.length <6){
+            return res.status(400).json({erro: 'Senha deve conter no mínimo 6 caracteres.'});
+            }
+    
+            const usuariosComMesmoEmail = await UsuarioModel.find({email: usuario.email});
+            if(usuariosComMesmoEmail && usuariosComMesmoEmail.length >0){
+                return res.status(400).json({erro: 'Email já cadastrado.'});
             }
 
-        if(!usuario.senha || usuario.senha.length <6){
-        return res.status(400).json({erro: 'Senha deve conter no mínimo 6 caracteres.'});
-        }
+            const image = await uploadImagemCosmic(req);
+            
+            const usuarioASerSalvo = {
+                nome: usuario.nome,
+                email: usuario.email,
+                senha: md5(usuario.senha),
+                avatar: image?.media?.url
+            };
 
-        const usuariosComMesmoEmail = await UsuarioModel.find({email: usuario.email});
-        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length >0){
-            return res.status(400).json({erro: 'Email já cadastrado.'});
-        }
-        
-        const usuarioASerSalvo = {
-            nome: usuario.nome,
-            email: usuario.email,
-            senha: md5(usuario.senha)
-        }
-        await UsuarioModel.create(usuarioASerSalvo);
-        return res.status(200).json({msg: 'Cadastro realizado com sucesso'});
+            await UsuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({msg: 'Cadastro realizado com sucesso'});
+});
+
+export const config = {
+    api: {
+        bodyParser: false
     }
-    return res.status(405).json({erro: 'Método informado não é válido.'});
 }
 
-export default conectaMongoDB(endpointCadastro);
+export default conectaMongoDB(handler);
