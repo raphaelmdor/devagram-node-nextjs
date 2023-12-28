9a94c39badc327003fbaf1da71619982d1d8dd5f
Raphael Monteiro de Oliveira Rebello <raphaelsiqueiraduarte@gmail.com>
2023-12-28 15:37:09 -0300

    api de like e deslike

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -3,17 +3,44 @@ import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
 import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
 import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 import { UsuarioModel } from "@/models/UsuarioModel";
-import { NextRequest } from "next/server";
 import { PublicacaoModel } from "@/models/PublicacaoModel";
 
 const likeEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg> | any) => {
     try{
         if(req.method === 'PUT'){
             //o id da publicação vem no query, por isso chamamos o req?.query
+            //achando a id da publicação
             const {id} = req?.query;
             const publicacao = await PublicacaoModel.findById(id);
+            if(!publicacao){
+                return res.status(400).json({erro:"Publicação não encontrada."});
+            }
+
+            //id do usuário que está curtindo a foto
+            const {userId} = req?.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if (!usuario) {
+                return res.status(400).json({erro:"Usuário não encontrado."});
+            }
+
+            //administrando os likes
+            const indexDoUsuarioNoLike = publicacao.likes.findIndex((e: any) => e.toString() === usuario._id.toString());
+            
+            //se o index for -1 é sinal de que ele não curte a foto
+            if(indexDoUsuarioNoLike != -1){
+                publicacao.likes.splice(indexDoUsuarioNoLike, 1);
+                await PublicacaoModel.findByIdAndUpdate({_id: publicacao._id}, publicacao);
+                return res.status(200).json({msg: 'Publicação descurtida com sucesso.'})
+            }else{
+                //se o index for > -1 é sinal de que ele curte a foto
+                publicacao.likes.push(usuario._id);
+                await PublicacaoModel.findByIdAndUpdate({_id: publicacao._id}, publicacao);
+                return res.status(200).json({msg: 'Publicação curtida com sucesso.'}); 
+            }
+
         }
-        return res.status(405).json({erro: 'Método informado não é válido.'})
+
+        return res.status(405).json({erro: 'Método informado não é válido.'});
 
     }catch(e){
         console.log(e);
@@ -24,3 +51,4 @@ const likeEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPa
 export default validarTokenJWT(conectarMongoDB(likeEndpoint));
 
 //método PUT é utilizado para fazer atualização de dados.
+//splice é um método do array onde se remove uma peça do meio.
